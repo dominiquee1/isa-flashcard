c6579373584d481c522d9323dacfc5a106cfdb0d
dominiquee1 <isabelacardoso931@gmail.com>
2024-11-26 20:37:15 -0300

    Add files via upload

---
+++ flashcard-aula_10-Isabela/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Rival',
+    'Qual é o maior rival do Flamengo no futebol brasileiro??',
+    'Vasco da Gama'
+)
+
+criaCartao(
+    'Vitórias',
+    'Quantas vezes o Flamengo ganhou a Copa Libertadores da América?',
+    '2 vezes (1981 e 2019)'
+)
+
+criaCartao(
+    'Campeonato',
+    'Em que ano o Flamengo conquistou o Campeonato Brasileiro pela primeira vez?',
+    '1980'
+)
+
+criaCartao(
+    'Jogador',
+    'Qual jogador do Flamengo é conhecido como "Zico"?',
+    'Arthur Antunes Coimbra'
+)
